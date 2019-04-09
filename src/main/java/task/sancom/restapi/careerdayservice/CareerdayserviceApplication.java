e93978fe1f685e1d6e3c0653e751a460ac1cd6b4
fraponyo94 <fraponyo94@gmail.com>
2019-04-09 21:05:30 +0300

    Initial project Structure

---
+++ src/main/java/task/sancom/restapi/careerdayservice/CareerdayserviceApplication.java
@@ -0,0 +1,13 @@
+package task.sancom.restapi.careerdayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerdayserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerdayserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/task/sancom/restapi/careerdayservice/CareerdayserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package task.sancom.restapi.careerdayservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CareerdayserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
